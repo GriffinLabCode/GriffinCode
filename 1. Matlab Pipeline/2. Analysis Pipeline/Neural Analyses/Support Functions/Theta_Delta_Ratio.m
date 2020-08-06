eca074f0253bd4cb961387b55a77dc8df3c0d775
John Stout <john.j.stout.jr@gmail.com>
2020-08-06 14:43:50 -0400

    theta delta changed

--- 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Support Functions/Theta_Delta_Ratio.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Support Functions/Theta_Delta_Ratio.m
@@ -17,7 +17,7 @@
 % Theta_Delta_Ratio: ratio from theta to delta
 % ThetaDeltaZ:       stds of theta delta ratio
 %
-% written by Suhaas
+% written by Suhaas. Modified and organized by JS.
 
 function [theta_delta_rat,ThetaDeltaZ] = Theta_Delta_Ratio(LFP,theta_bandpass,delta_bandpass,srate)
 
@@ -43,7 +43,7 @@ inst_delta_power   = abs(hilbert(lfp_filtered_delta));
 
 % -- calculate ratio -- %
 %Theta_Delta_Ratio = lfp_filtered_theta./lfp_filtered_delta;
-theta_delta_rat = inst_delta_power./inst_theta_power;
+theta_delta_rat = inst_theta_power./inst_delta_power; % theta / delta - was delta/theta - JS change on 8/6/20
 
 % Z-score data to work in Std. Devs 
 ThetaDeltaZ = zscore(theta_delta_rat);
