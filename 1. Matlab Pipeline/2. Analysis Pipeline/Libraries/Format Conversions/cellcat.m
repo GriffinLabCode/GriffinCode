4ef5cd04865771b5ef97da2e5c6b9982e6bbe4da
John Stout <jstout@psych.udel.edu>
2020-10-26 14:12:32 -0400

    update

--- 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Format Conversions/cellcat.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Format Conversions/cellcat.m
@@ -1,29 +1,29 @@
 %% cellcat
-% fun_inputs: which orientation you want to collapse across the cell array
-% cat_apply: which form of concatenation to apply; 'horzcat' or 'vertcat'
+% direction: which orientation you want to collapse across the cell array
+% cat_method: which form of concatenation to apply; 'horzcat' or 'vertcat'
 %
 % -- OUTPUTS -- %
 % fun_out: output of concatenation
 % 
 % written by John Stout on 10/15/2020
 
-function [fun_out] = cellcat(cell_array,cat_apply,fun_input)
+function [fun_out] = cellcat(cell_array,cat_method,direction)
 
 % take arguments and function and change for application
-fun_argument = str2double(fun_input);
-fun_apply = str2func(cat_apply);
+%fun_argument = str2double(fun_input);
+fun_apply = str2func(cat_method);
 
 % must decide which way to loop
-if contains(fun_input,'row')
+if contains(direction,'row')
     looper = size(cell_array,2);
-elseif contains(fun_input,'col')
+elseif contains(direction,'col')
     looper = size(cell_array,1);
 end
 
 for i = 1:looper
-    if contains(fun_input,'row')
+    if contains(direction,'row')
         fun_out{i} = fun_apply(cell_array{:,i});
-    elseif contains(fun_input,'col')
+    elseif contains(direction,'col')
         fun_out{i} = fun_apply(cell_array{i,:});
     end
 end
---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Other Plotting Functions/cumulativeDensity.m
@@ -0,0 +1,31 @@
+%% cumulative density figure
+% data: cell array containing data
+% color: cell array containing color
+% x_label: string label
+% title_label: string
+% paired: 1, y, or Y, for paired kstest. Nothing or anything else for
+% two-sample
+
+
+function [p,kstat] = cumulativeDensity(data,color,x_label,title_label,saveName,paired)
+
+    figure('color','w'); hold on;
+    h1 = cdfplot(data{1});
+    h1.Color = color{1}; %[.3 .3 .3];
+    h2 = cdfplot(data{2});
+    h2.Color = color{2}; %[0 .6 0];
+    grid off
+    ylabel('Cumulative Frequency')
+    xlabel(x_label)
+    title(title_label)
+    set(gcf,'Position',[300 250 350 300])
+    
+    if exist('paired') & ( paired == 1 | contains(paired,[{'y'},{'Y'}]) )
+        [h,p,kstat]=kstest(data{1},data{2});        
+    else
+        [h,p,kstat]=kstest2(data{1},data{2});
+    end
+    
+    if exist('saveName') 
+    print('-painters',[saveName,'.eps'],'-depsc','-r0')
+    end
