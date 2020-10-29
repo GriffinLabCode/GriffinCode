9303f61105cf689445fe3f73f3c41d847b965e06
John Stout <jstout@psych.udel.edu>
2020-10-29 12:14:08 -0400

    Update getLFPdata.m

--- 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/getLFPdata.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/getLFPdata.m
@@ -23,7 +23,7 @@ cd(datafolder)
 load(csc_name,'Samples','Timestamps');
 try load(csc_name,'SampleFrequencies'); catch; end % try to load sample frequencies for srate
 
-% convert data
+% convert data - do not linspace
 [lfpTimes, lfp] = interp_TS_to_CSC_length_non_linspaced(Timestamps, Samples);   
 
 if exist('SampleFrequencies')
