241ce75d7bb1899b9c8441316fd86fb8069bd6b9
John Stout <john.j.stout.jr@gmail.com>
2020-10-30 08:58:40 -0400

    updates

--- 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/downSampleLFPdata.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/downSampleLFPdata.m
@@ -1,39 +1,37 @@
 %% downSampleLFPdata
 %
-% this function downsamples lfp data
+% this function downsamples lfp vector. This could be useful for
+% downsampling your converted lfp vector to a lower frequency. For example,
+% if you want to estimate theta entrainment, it could be useful to
+% downsample from 2000hz to 125hz. This would accomplish that task
 %
 % -- INPUTS -- %
-% datafolder: string directory
-% csc_name: string of name of csc
-% target_rate: down sample target rate (say 2000hz), a double variable
+% lfp_data: vector of lfp data sampled at the current rate
+% lfp_times: vector of lfp timestamps sampled at the current rate
+% srate: current sampling rate
+% target_rate: target sampling rate (ie try 125hz)
 %
 % -- OUTPUTS -- %
+% lfp_ds: downsampled vector of lfp data
+% times_ds: downsampled vector of times
+%
+% written by John Stout
 
-function [Samples, Timestamps] = downSampleLFPdata(datafolder,csc_name,target_rate,saveName);
+function [lfp_ds, times_ds] = downSampleLFPdata(lfp_data,lfp_times,srate,target_rate)
 
-% load csc data
-load(csc_name);
+if srate ~= target_rate
+    disp('Sampling rate does not match the target rate, therefore data will be down-sampled...')
+    % get the downsampling rate divisor
+    %target_rate = 2000;
+    [divisor] = find_downsample_rate(srate,target_rate);
 
-% get current rate
-srate = getLFPsrate(Timestamps,Samples);
-
-% get the downsampling rate divisor
-%target_rate = 2000;
-[divisor] = find_downsample_rate(srate,target_rate);
-
-% downsample data
-samples_ds = []; times_ds = [];
-samples_ds = Samples(:,1:divisor:end);
-times_ds   = Timestamps(:,1:divisor:end);
-
-% format data for output
-Samples = []; Timestamps = [];
-Samples = samples_ds;
-Timestamps = times_ds;
-
-% save data
-if exist('saveName') 
-    save(saveName,'Samples','Timestamps')
+    % downsample data
+    lfp_ds = []; times_ds = [];
+    lfp_ds   = lfp_data(1:divisor:end);
+    times_ds = lfp_times(1:divisor:end);
+    
+else
+    disp('Sampling rate matches the target rate, therefore data was not down-sampled')
 end
 
 end
--- 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/rateReducerLFP.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/rateReducerLFP.m
@@ -1,17 +1,27 @@
-%% downSampleLFPdata
+%% reduce sampling rate of raw lfp
 %
-% this function downsamples lfp vector
+% this function downsamples raw lfp and timestamps BEFORE conversion. This
+% function is identical to the rate reducer neuralynx application. This
+% could be useful if you accidentally sampled at a really high srate (say
+% 30,000 samples/sec), but you want 2000hz.
 %
 % -- INPUTS -- %
 % datafolder: string directory
 % csc_name: string of name of csc
 % target_rate: down sample target rate (say 2000hz), a double variable
+% saveName: name of data you want saved as. If this is empty, the variable
+%               will automatically be saved as your 'csc_name_rateReduced'
 %
 % -- OUTPUTS -- %
+% Samples: matrix of 512xN samples, in raw, downsampled format.
+% Timestamps: vector of N timestamps, in raw, downsampled format
+%
+% written by John Stout
 
-function [Samples, Timestamps] = downSampleLFPdata(datafolder,sra);
+function [Samples, Timestamps] = rateReducerLFP(datafolder,csc_name,target_rate,saveName);
 
 % load csc data
+cd(datafolder);
 load(csc_name);
 
 % get current rate
@@ -19,6 +29,8 @@ srate = getLFPsrate(Timestamps,Samples);
 
 if srate ~= target_rate
     disp('Sampling rate does not match the target rate, therefore data will be down-sampled...')
+    disp('WARNING - rate reduction does not always provide you with values that match another LFP that was down-sampled during recording')
+    
     % get the downsampling rate divisor
     %target_rate = 2000;
     [divisor] = find_downsample_rate(srate,target_rate);
@@ -39,6 +51,8 @@ end
 % save data
 if exist('saveName') 
     save(saveName,'Samples','Timestamps')
+else
+    save([csc_name,'_rateReduced'])
 end
 
 end
