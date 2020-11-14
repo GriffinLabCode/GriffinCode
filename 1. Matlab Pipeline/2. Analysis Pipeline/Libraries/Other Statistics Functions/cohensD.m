30b2d44aecde9aef6bfe943ee6659b6aaaee7931
John Stout <john.j.stout.jr@gmail.com>
2020-11-14 09:30:57 -0500

    cohens d function added

---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Other Statistics Functions/cohensD.m
@@ -0,0 +1,11 @@
+%% cohens d
+% calculates effect size using cohens D
+%d = (M2 - M1) ? ?((SD12 + SD22) ? 2)
+%
+% written by John Stout using https://www.socscistatistics.com/effectsize/default3.aspx
+
+function [d] = cohensD(x,y)
+
+    d = (nanmean(y) - nanmean(x)) / sqrt(((nanstd(x)^2) + (nanstd(y)^2))/2);
+
+end
