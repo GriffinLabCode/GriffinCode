fb13b0e763a74f82d55bb88336f599b7bd85f608
John Stout <john.j.stout.jr@gmail.com>
2021-02-11 10:18:12 -0500

    updates

---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Chronux Toolbox/spectral_analysis/helper/getCustomParams.m
@@ -0,0 +1,18 @@
+%% custom parameters
+%
+% JS custom parameters
+
+function [] = getCustomParams()
+
+params.tapers    = [3 5];
+params.trialave  = 0;
+params.err       = [2 .05];
+params.pad       = 0;
+params.fpass     = [0 100]; % [1 100]
+params.movingwin = [0.5 0.01]; %(in the form [window winstep] 500ms window with 10ms sliding window Price and eichenbaum 2016 bidirectional paper
+params.Fs        = []; %data1.SampleFrequencies(1);
+params.cleanFreq = [58 62];
+
+disp('Remember to define params.Fs as your sampling rate')
+
+
