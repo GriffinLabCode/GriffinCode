69a7fe0fc4298ec6479ec65d191f46589732380b
John Stout <john.j.stout.jr@gmail.com>
2021-03-25 23:15:46 -0400

    Update getCustomParams.m

--- 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Chronux Toolbox Updated/spectral_analysis/helper/getCustomParams.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Libraries/Chronux Toolbox Updated/spectral_analysis/helper/getCustomParams.m
@@ -15,7 +15,8 @@ params.Fs        = []; %data1.SampleFrequencies(1);
 params.cleanFreq = [58 62];
 %}
 
-params.tapers = [5 9];
+% use 3 and 5 for tapers - it really seems like the best middle ground
+params.tapers = [3 5];
 params.pad = 0;
 params.Fs = [];
 params.fpass = [0 100];
