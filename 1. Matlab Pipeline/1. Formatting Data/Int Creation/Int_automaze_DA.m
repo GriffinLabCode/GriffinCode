f4eb1cc6ce008c034414fac8c3ea0ddad942ed7a
John Stout <john.j.stout.jr@gmail.com>
2021-11-04 13:03:49 -0400

    updates

--- 1. Matlab Pipeline/1. Formatting Data/Int Creation/Int_automaze_DA.m
+++ 1. Matlab Pipeline/1. Formatting Data/Int Creation/Int_automaze_DA.m
@@ -14,8 +14,8 @@ cd(datafolder);
 clearvars -except datafolder datafolderNew
 
 % load in events
-load('Events')
-[x,y,t] = getVTdata(datafolder,'interp','VT1.mat');
+load('Events','EventStrings','TimeStamps')
+[pos_x,pos_y,pos_t] = getVTdata(datafolder,'interp','VT1.mat');
 
 %% get sequence of events
 
@@ -67,59 +67,44 @@ for i = 1:numTrials-1
         Int(i+1,4) = 1;
     end
 end
-percentCorrect = (((numTrials)-(sum(Int(:,4))))/(numTrials))*100;
+percentCorrect = (((numTrials)-(sum(Int(:,4))))/(numTrials-1))*100;
 
-save('Int_IR','Int')
-%{
-% get video tracking data
-missing_data = 'interp'; % this could be 'exclude' or 'ignore'
-vt_name = 'VT1.mat';
-[ExtractedX,ExtractedY,TimeStamps] = getVTdata(datafolder,missing_data,vt_name);
-
-% define some old school variables
-pos_x = ExtractedX; pos_y = ExtractedY; pos_t = TimeStamps;
-
-% Define the beginning and end of the session
-start  = TimeStamps(1);
-finish = TimeStamps(end); 
-Int = [];
-
-% Define the Int variable
-[Int] = whereishe_master(pos_x,pos_y,pos_t);
-
-% set up for populating correct/incorrect column 4
-ind         = find(pos_t>=start & pos_t<=finish);
-pos_t1=pos_t(ind); pos_x1=pos_x(ind); pos_y1=pos_y(ind);
-Int_ind     = find(Int(:,1)>start & Int(:,8)<finish);
-starttrials = Int_ind(1,1);
-endtrials   = Int_ind(end);
-Int         = Int(1:endtrials,:);
-
-% Populate column 4 of the Int variable 
-% 0 = Correct, 1 = Incorrect
-numtrials = size(Int,1);
-for i = 1:numtrials-1
-    if Int(i,3) == 1 && Int(i+1,3) == 0 || Int(i,3) == 0 && Int(i+1,3) == 1
-        Int(i+1,4) = 0;
-    else
-        Int(i+1,4) = 1;
-    end
-end
-percentCorrect = (((numtrials/2)-(sum(Int(:,4))/2))/(numtrials/2))*100;
-
-% display progress
-C = [];
-C = strsplit(datafolder,'\');
-X = [];
-X = [C{end},' behavioral accuracy = ',num2str(percentCorrect),'%'];
-disp(X);
 
 % check Int for timing-position accuracy
 question = 'Would you like to confirm your int file is correct? [Y/N] ';
 answer   = input(question,'s');
 
 if contains(answer,'Y') | contains(answer,'y')
-    checkInt;
+
+    % number of trials
+    numTrials = size(Int,1);
+
+    p1 = []; p2 = [];
+    for i = 1:numTrials
+        figure('color','w'); hold on;    
+        p1 = plot(pos_x,pos_y,'Color',[.8 .8 .8]); 
+        p1.Annotation.LegendInformation.IconDisplayStyle = 'off';
+
+        % get position data on a trial-by-trial basis
+        x_trial = pos_x(pos_t >= Int(i,5) & pos_t <= Int(i,6));
+        y_trial = pos_y(pos_t >= Int(i,5) & pos_t <= Int(i,6));
+        
+        plot(x_trial,y_trial,'r','LineWidth',2)
+
+        % correct any issues
+        question = 'Keep trial? [Y/N] / [y/n] ';
+        answer   = input(question,'s');
+
+        if contains(answer,[{'N'} {'n'}])
+            remData(i) = 1;
+        else
+            remData(i) = 0;
+        end
+
+        %pause;
+        close;
+    end 
+    remData = logical(remData);
     
     % remove data selected by user
     Int(remData,:)=[];
@@ -132,7 +117,7 @@ if contains(answer,'Y') | contains(answer,'y')
             Int(i+1,4) = 1;
         end
     end
-    percentCorrect = (((numtrials/2)-(sum(Int(:,4))/2))/(numtrials/2))*100;
+    percentCorrect = (((numTrials)-(sum(Int(:,4))))/(numTrials-1))*100;
 
     % display progress
     C = [];
@@ -140,23 +125,14 @@ if contains(answer,'Y') | contains(answer,'y')
     X = [];
     X = [C{end},' behavioral accuracy = ',num2str(percentCorrect),'%'];
     disp(X);
-
 else
     disp('IT IS RECOMMENDED that you check your int file as missing data will be stored as a non-existing trial');
 end
 
-% save data
-question = 'Are you satisfied with the Int file and ready to save? [Y/N] ';
-answer   = input(question,'s');
 
-if contains(answer,'Y') | contains(answer,'y')
-    cd(datafolder);
-    
-    % have user define a name
-    question    = 'Please enter an Int file name: ';
-    IntFileName = input(question,'s');
-    
-    % save
-    save(IntFileName,'Int');
-end
-%}
+
+save('Int_IR','Int')
+
+
+
+
--- Experiment Control/R21/Experimental Code/step3part3_memoryVbowlCoherence.m
+++ Experiment Control/R21/Experimental Code/step3part3_memoryVbowlCoherence.m
@@ -12,7 +12,7 @@ rats{7} = '21-22';
 for i = 1:length(rats)
     try
         cd(['X:\01.Experiments\R21\',rats{i},'\step3-definingCoherenceThresholds']);
-        dataIN{i,:} = load(['CoherenceDistribution',rats{i}],'coh');
+        dataIN{i,:} = load(['CoherenceDistribution',rats{i}],'coh','LFP1name','LFP2name');
     end
 end
 
@@ -34,12 +34,138 @@ distType   = 'normal';
 
 % so I need to get rats performing the CA task and eating on all trials
 
+%%
+clearvars -except cohData dataIN rats
+
+% now get data for every session and compute coherence like for 21-13 below
+%Datafolders = 'X:\01.Experiments\R21';
+
+for i = 1:length(rats)
+    % get datafolders (session names) into a cell array
+    Datafolders = ['X:\01.Experiments\R21\',rats{i},'\Sessions'];
+    dir_content = [];
+    dir_content = dir(Datafolders);
+    dir_content = extractfield(dir_content,'name');
+    remIdx = contains(dir_content,'.mat') | contains(dir_content,'.');
+    dir_content(remIdx)=[];
+
+    % define LFP names
+    hpcName = dataIN{i}.LFP1name;
+    pfcName = dataIN{i}.LFP2name;
+
+    try
+        % loop across
+        for sessi = 1:length(dir_content)
+            clear lfp_pf_cp lfp_hc_cp lfp_pfc lfp_hpc lfp_ts Int
+
+            % define datafolder
+            datafolder = [Datafolders,'\',dir_content{sessi}];
+
+            % load INT
+            cd(datafolder);
+            load('Int_IR')
+            
+            % load lfp
+            [lfp_pfc,lfp_ts,srate] = getLFPdata(datafolder,pfcName);
+            [lfp_hpc,~,srate_hc] = getLFPdata(datafolder,hpcName);
+
+            % get data into usable format
+            lfp_pfc = lfp_pfc{:};
+            lfp_hpc = lfp_hpc{:};
+            lfp_ts  = lfp_ts{:};
+
+            % get times and lfp around cp
+            numTrials = size(Int,1);
+            for triali = 1:numTrials
+                idx = find(lfp_ts > (Int(triali,5)) & lfp_ts < (Int(triali,6)));
+                lfp_pf_cp{triali} = lfp_pfc(idx);
+                lfp_hc_cp{triali} = lfp_hpc(idx);
+            end
+
+            %% first, create coherence across frequencies plot
+            % randomly pull 500ms of data, 1000 times
+            %rng('shuffle')
+            %outRand = randi([1 10],1); % was 3
+            rng(3);
+            k = 1000;
+            clear lfp_pf_rand lfp_hc_rand lfp_pf_det lfp_hc_det coh
+            for triali = 1:numTrials
+                % define instances to pull from, min to max
+                % subtract 1/2 second, give some leeway (eg add 1/4 sec)
+                idxPull = 1:length(lfp_pf_cp{triali})-((srate));
+                % define your start idx's
+                try 
+                    randPull = randperm(length(idxPull),k);
+                catch
+                    randPull = randperm(length(idxPull),length(idxPull));
+                end
+                % get data
+                for n = 1:length(randPull)
+                    % pull data
+                    lfp_pf_rand = lfp_pf_cp{triali}(randPull(n):randPull(n)+srate/2);
+                    lfp_hc_rand = lfp_hc_cp{triali}(randPull(n):randPull(n)+srate/2);
+                    % detrend
+                    lfp_pf_det = detrend(lfp_pf_rand);
+                    lfp_hc_det = detrend(lfp_hc_rand);
+                    % coherence
+                    fpass = [1:20];
+                    window = []; noverlap = [];
+                    [coh{triali}{n},f] = mscohere(lfp_hc_det,lfp_pf_det,window,noverlap,fpass,srate);
+                    disp(['finished with iteration',num2str(n)])
+                end
+                disp(['finished with trial ',num2str(triali)])
+            end
+
+            % first, check the coherence distribution, is 6-12hz similar to what it was
+            % using real-time coh methods?
+
+            % second, perform averaging across 6-12hz bands, generate probability
+            % density function and compare with real-time coh methods. Is there a
+            % 'memory' bump, like Fernandez ruiz 2019 found with SWRs?
+            coh_mat = [];
+            for n = 1:length(coh)
+                coh_mat{n} = vertcat(coh{n}{:});
+            end
+            coh_mat_avg = []; coh_mat_trial = []; coh_mat_tAvg = []; coh_mat_tSEM = [];
+            coh_mat_avg = cellfun(@nanmean,coh_mat,'UniformOutput',false);
+            coh_mat_trial = vertcat(coh_mat_avg{:});
+            coh_mat_tAvg = nanmean(coh_mat_trial);
+            coh_mat_tSEM = stderr(coh_mat_trial,1);
+
+            % store
+            dataOUT{i}.fXc_avg = coh_mat_tAvg;
+            dataOUT{i}.fXc_sem = coh_mat_tSEM;
+
+            %% next get the distribution averaged across 6-12hz
+
+            % now compare 6-12hz distributions
+            clear coh_mat_theta
+            for n = 1:length(coh_mat)
+                idxTheta = find(f > 6 & f < 12);
+                % select 600
+                %randPull = randperm(length(coh_mat{i}),400);
+                % average across frequencies, then across iterations
+                coh_mat_theta{n} = nanmean(coh_mat{n}(:,idxTheta),2);
+            end
+            cohMatAll = []; randPull = [];
+            cohMatAll = vertcat(coh_mat_theta{:});
+
+            randPull = randperm(length(cohMatAll),600);
+            coh_mat_use = cohMatAll(randPull);
+
+            dataOUT{i}.thetaCoh = coh_mat_use;
+        end       
+    end
+    disp(['Finished with ' rats{i}])
+end
+
+%{     
 %% 21-13
 % rat did well, so gonna test if coherence differs bw bowl and CP
 datafolder = 'X:\01.Experiments\R21\21-13\2021-11-02_15-07-45 - use';
 cd(datafolder);
 load('Int_IR');
-mazeData = load('mazeData');
+mazeData = load('MazeData');
 cd('X:\01.Experiments\R21\21-13\step3-definingCoherenceThresholds')
 cohDistBowl = load('CoherenceDistribution21-13');
 cd(datafolder);
@@ -147,3 +273,4 @@ dataLabels{1} = 'Bowl distribution 6-12hz'; dataLabels{2} = 'Trial distribution 
 distType   = 'normal';
 [y,a] = plotCurves(data2plot,xRange,colors,dataLabels,distType);
 
+%}
