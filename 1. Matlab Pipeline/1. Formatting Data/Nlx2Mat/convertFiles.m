8096e4ddbf1a4783e4ea8c908f165f78992cbb54
John Stout <john.j.stout.jr@gmail.com>
2021-11-30 19:17:26 -0500

    updates

--- 1. Matlab Pipeline/1. Formatting Data/Nlx2Mat/convertFiles.m
+++ 1. Matlab Pipeline/1. Formatting Data/Nlx2Mat/convertFiles.m
@@ -1,20 +1,15 @@
 %% convert session files
-% This script converts neuralynx files to a usable matlab format
+% This function converts neuralynx files to a usable matlab format
 
-%% Define data folder manually and add necessary path to working directory
 
-% clear workspace
-clear; clc
-
-% designate folder where session-specfic files are located (most important)
-datafolder = pwd;
-cd(datafolder)
+function [] = convertFiles(datafolder)
+cd(datafolder);
 
 % define which CSCs to convert. Maybe you only want to convert CSCs 1 and
 % 3. If you want CSCs 1:16: numCSC = 1:16;
 %numCSC = [3,10,14]; % ** If your CSC are numbered, do this and comment below
 %strCSC = [{'PFC_11'} {'PFC_10'} {'HPC_43'} {'HPC_41'} {'HPC_33'} {'HPC_31'}]; % if your csc are strings, do this and comment above
-%strCSC = [{'PFC_red'} {'PFC_blue'} {'HPC_red'} {'HPC_green'} {'HPC_blue'} {'HPC_black'}]; % if your csc are strings, do this and comment above
+strCSC = [{'PFC_red'} {'PFC_blue'} {'HPC_red'} {'HPC_green'} {'HPC_blue'} {'HPC_black'}]; % if your csc are strings, do this and comment above
 
 %strCSC = [{'PFC_red'} {'PFC_blue'} {'HPC_red'} {'HPC_clear'} {'HPC_blue'} {'HPC_black'} {'REF'}]; % if your csc are strings, do this and comment above
 %strCSC = [{'PFC_white'} {'PFC_blue'} {'HPC_white'} {'HPC_clear'} {'HPC_blue'} {'HPC_green'}]; % if your csc are strings, do this and comment above
