ecbd0bcef92e57f4f8429ad9a81a2fb3fcc3be6a
John Stout <john.j.stout.jr@gmail.com>
2022-01-25 11:56:16 -0500

    updates

--- 1. Matlab Pipeline/2. Analysis Pipeline/Behavioral Analyses/VTE Code/get_oopsTrials.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Behavioral Analyses/VTE Code/get_oopsTrials.m
@@ -17,16 +17,16 @@ function [oopsTrial] = oopsTrials(x,y)
 %xmax = 450;
 
 % identify parameters for left choice point
-minY = 223; addY = abs(minY-300);
-minX = 390; addX = abs(minX-450);
+minY = 219; addY = abs(minY-300);
+minX = 410; addX = abs(minX-450);
 lCP_fld = [minX minY addX addY]; %[180 215 395 50]; % x,y (first corner) , x,y (second right top corner)
 
 xv_lCP = [lCP_fld(1)+lCP_fld(3) lCP_fld(1) lCP_fld(1) lCP_fld(1)+lCP_fld(3) lCP_fld(1)+lCP_fld(3)];
 yv_lCP = [lCP_fld(2) lCP_fld(2) lCP_fld(2)+lCP_fld(4) lCP_fld(2)+lCP_fld(4) lCP_fld(2)];
 
 % same for right choice point
-minY = 105; addY = abs(minY-193);
-minX = 390; addX = abs(minX-450);
+minY = 105; addY = abs(minY-196);
+minX = 410; addX = abs(minX-450);
 rCP_fld = [minX minY addX addY]; %[180 215 395 50]; % x,y (first corner) , x,y (second right top corner)
 
 xv_rCP = [rCP_fld(1)+rCP_fld(3) rCP_fld(1) rCP_fld(1) rCP_fld(1)+rCP_fld(3) rCP_fld(1)+rCP_fld(3)];
@@ -37,12 +37,12 @@ yv_rCP = [rCP_fld(2) rCP_fld(2) rCP_fld(2)+rCP_fld(4) rCP_fld(2)+rCP_fld(4) rCP_
 for triali = 1:length(x)
     
     % check if the rat is in both areas for this trial
-    [in_left]  = inpolygon(x{triali},y{triali},xv_lCP,yv_lCP);
-    [in_right] = inpolygon(x{triali},y{triali},xv_rCP,yv_rCP);
+    [in_left,on_left]  = inpolygon(x{triali},y{triali},xv_lCP,yv_lCP);
+    [in_right,on_right] = inpolygon(x{triali},y{triali},xv_rCP,yv_rCP);
     
     % find instances where the rat is in both
-    idxInLeft  = find(in_left==1);
-    idxInRight = find(in_right==1);
+    idxInLeft  = find(in_left==1 | on_left==1);
+    idxInRight = find(in_right==1 | on_right==1);
     
     % 
     if isempty(idxInLeft) == 0 && isempty(idxInRight)==0
@@ -54,6 +54,7 @@ for triali = 1:length(x)
 end
 
 %{
+plot(x,y,'r')
 hold on;
 rectangle ('position', rCP_fld);  % right reward field
 rectangle ('position', lCP_fld);  % left reward field
--- 1. Matlab Pipeline/2. Analysis Pipeline/Behavioral Analyses/VTE Code/get_session_IdPhi_sb2cp.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Behavioral Analyses/VTE Code/get_session_IdPhi_sb2cp.m
@@ -6,7 +6,7 @@
 % this function takes one days worth of data and computes IdPhi across
 % trials. There are many outputs
 
-function [IdPhi,x_data,y_data,ts_data,ExtractedX,ExtractedY,TimeStamps,timeSpent,x_data_raw,y_data_raw,ts_data_raw] = get_session_IdPhi(datafolder,int_name,vt_name,missing_data,middleStemPosition,stemOrientation,preSmooth,mazeLoc)
+function [IdPhi,x_data,y_data,ts_data,ExtractedX,ExtractedY,TimeStamps,timeSpent,x_data_raw,y_data_raw,ts_data_raw,remEmpty] = get_session_IdPhi(datafolder,int_name,vt_name,missing_data,middleStemPosition,stemOrientation,preSmooth,mazeLoc)
 
 % load vt data
 %missing_data = 'interp';
@@ -28,9 +28,9 @@ end
 % get data from sb entry to cp exit - I want to know what they were doing
 % at CP exit
 for i = 1:numTrials
-    x_data_raw{i}  = ExtractedX(TimeStamps >= (Int(i,1)-(10*1e6)) & TimeStamps <= Int(i,6));
-    y_data_raw{i}  = ExtractedY(TimeStamps >= (Int(i,1)-(10*1e6)) & TimeStamps <= Int(i,6));
-    ts_data_raw{i} = TimeStamps(TimeStamps >= (Int(i,1)-(10*1e6)) & TimeStamps <= Int(i,6));
+    x_data_raw{i}  = ExtractedX(TimeStamps >= (Int(i,1)-(5*1e6)) & TimeStamps <= Int(i,6));
+    y_data_raw{i}  = ExtractedY(TimeStamps >= (Int(i,1)-(5*1e6)) & TimeStamps <= Int(i,6));
+    ts_data_raw{i} = TimeStamps(TimeStamps >= (Int(i,1)-(5*1e6)) & TimeStamps <= Int(i,6));
 end
 
 % middle position of stem
@@ -72,11 +72,11 @@ end
 
 % pre smoothing
 if preSmooth == 1  
-    x_data_og = x_data; x_data = [];
-    y_data_og = y_data; y_data = [];
+    x_data_og = x_data; %x_data = [];
+    y_data_og = y_data; %y_data = [];
     for i = 1:numTrials
-        x_data{i}  = smoothdata(x_data_og{i},'gauss',30);
-        y_data{i}  = smoothdata(y_data_og{i},'gauss',30);  
+        %x_data{i}  = smoothdata(x_data_og{i},'gauss',10);
+        %y_data{i}  = smoothdata(y_data_og{i},'gauss',10);  
     end    
 end
 
@@ -87,7 +87,7 @@ vt_srate      = ceil(1/mean(diff(TimeStamps/1e6))); % rounded to a integer for e
 display       = 0;
 
 for i = 1:numTrials
-    [IdPhi(i),dphi{i}] = IdPhi_RedishFun(x_data{i},y_data{i},window_sec,postSmoothing,vt_srate,display);
+    [IdPhi(i),dphi{i}] = IdPhi_RedishFun(x_data_og{i},y_data_og{i},window_sec,postSmoothing,vt_srate,display);
 end
 
 end
