99b2613e98b3f110774f7b285457e079f6a12753
John Stout <john.j.stout.jr@gmail.com>
2022-01-26 13:55:35 -0500

    tdh

---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Behavioral Analyses/Support Functions/getAction.m
@@ -0,0 +1,4 @@
+%% getAction
+% getAction will be a function used to assign action points in position
+% data. A clear example of this is the turn-around point in VTE.
+% It will be fed a datafolder
---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Extract LFP/getEventTriggeredLFP.m
@@ -0,0 +1,6 @@
+%% Event triggered LFP
+% this code will provide a method to extract LFP around some time stamp
+% inputs using a timeAround variable. Can be fed into spike triggered
+% average code.
+
+
