0b1df697754f8c013d98290a5afc064ff2f391d1
John Stout <john.j.stout.jr@gmail.com>
2022-06-10 13:32:29 -0400

    Spike field coherence
    Finalizing spike field coherence code

---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Entrainment Code/getSpikeFieldCoherence.m
@@ -0,0 +1,60 @@
+%% spike field coherence function
+%
+% % the output is an alternative to entrainment. Can be interpreted as the
+% strength of phase locking at each frequency. It does not have quite the
+% precision of the entrainment code which can tell you exact theta phase.
+% But has precision in the frequency domain which would take forever to do
+% with the entrainment code.
+%
+% -- INPUTS -- %
+% lfp: vector of LFP used for spike indexing
+% spikeLFPidx: an index, pointing to LFP values (lfp variable) that
+%               correspond to spike timestamps. This is tricky and time
+%               consuming. Needs to be done outside of this code.
+% freq: range of frequencies
+% nCycle: number of cycles for morlet wavelet convolution. Should be
+%           between 4 and 12. Higher values = greater frequency precision
+%           at the expense of temporal precision. Default = 6.
+% srate: sampling rate for your data
+%
+% -- OUTPUTS -- %
+% sfc: spike field coherence across the range of frequencies provided
+%
+% This code was adopted from MxC: MATLAB for brain and cognitive
+% scientists by John Stout and confirmed for accuracy with their data.
+
+function [sfc] = getSpikeFieldCoherence(lfp,spikeLFPidx,freq,nCycle,srate)
+% convert data to single
+lfp = single(lfp);
+
+if exist('freq')==0 
+    disp('Did not detect frequency input, default to log space')
+    freq = logspace(0,2); % 10^0 to 10^2: 1:100
+elseif isempty(freq)==1
+    disp('Did not detect frequency input, default to log space')
+    freq = logspace(0,2); % 10^0 to 10^2: 1:100
+end
+
+if exist('nCycle')==0 
+    disp('Did not detect # cycles input, default to 6')
+    nCycle = 6; 
+elseif isempty(nCycle)==1
+    disp('Did not detect # cycles input, default to 6')
+    nCycle = 6;
+end
+
+for wavei = 1:length(freq)
+    % get the analytic signal for morlet wavelet convolution
+    as = getMorletWaveletConv(lfp,freq(wavei),nCycle,srate);
+    % get the phase angle of each spike
+    angles = angle(as(spikeLFPidx)); 
+    % spike field coherence is the length of the averaged vector
+    sfc(wavei) = abs(mean(exp(1i*angles)));
+end
+%{
+figure('color','w')
+plot(freq,sfc);
+%}
+
+
+
---
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Entrainment Code/getSpikeLFPidx.m
@@ -0,0 +1,37 @@
+%% get spike time index
+%
+% code designed to extract LFP for each spike. It should be noted that you
+% do not need to put your entire LFP trace into this code. However, you do
+% need to make sure that your LFP and spike timestamps trace are consistent
+% with the time boundaries. For example, if you have spike timestamps from
+% time point 0-10s, but lfp from 1-10s, its going to force your spike
+% timestamp data from range 0-1s into lfp data at 1s.
+%
+% If you are interested in delay period spike times, get spike times during
+% the delay, get LFP during the delay, then input that code. It will save
+% you a lot of processing time.
+%
+% -- INPUTS -- %
+% lfp: vector of lfp
+% spikeTimes: spike timestamps that you want to get LFP values for
+%
+% -- OUTPUTS -- %
+% spikeLFPidx: an index of spike-LFP values. Use this to get LFP values
+% spikeLFPval: LFP per each spike
+%
+% written by John Stout
+
+function [spikeLFPidx,spikeLFPval] = getSpikeLFPidx(lfp,spikeTimes)
+% do some reorienting
+lfp = change_row_to_column(lfp);
+spikeTimes = change_row_to_column(spikeTimes)';
+
+% get index of LFP
+spikeLFPidx = [];
+spikeLFPidx = dsearchn(lfp,spikeTimes);
+
+% Get lfp
+spikeLFPval = lfp(spikeTimes);
+
+
+
