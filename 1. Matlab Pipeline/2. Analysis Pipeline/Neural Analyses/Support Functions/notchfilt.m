4c2d15f4978e47aa5f03843c07498cb9917382a4
John Stout <john.j.stout.jr@gmail.com>
2022-10-14 22:20:05 -0400

    cleaning up notchfilter
    Works great now!!

--- 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Support Functions/notchfilt.m
+++ 1. Matlab Pipeline/2. Analysis Pipeline/Neural Analyses/Support Functions/notchfilt.m
@@ -4,10 +4,22 @@
 % https://www.nature.com/articles/s42003-021-02558-4.pdf
 %
 
-function [filtLFP] = notchfilt(lfp_data,Fs)
+function [filtLFP] = notchfilt(lfp_data,Fs,fpass)
+
+if exist('fpass')==0
+    % default fpass
+    fpass = [55 65];
+    disp('Defaulting fpass to 55-65Hz - change if necessary')
+end
+
+% redefine bandpass filtering variables
+lowPass = fpass(1);
+highPass = fpass(2);
+diffPass = fpass(2)-fpass(1);
+
 % use a notch filter to remove 60hz
-d = designfilt('bandstopiir','FilterOrder',2, ...
-               'HalfPowerFrequency1',59,'HalfPowerFrequency2',61, ...
+d = designfilt('bandstopiir','FilterOrder',diffPass, ...
+               'HalfPowerFrequency1',lowPass,'HalfPowerFrequency2',highPass, ...
                'DesignMethod','butter','SampleRate',Fs);                     
 filtLFP = filtfilt(d,lfp_data);           
            
